cfec69c2eb115123223594aeb8a8b20fcdd0fcab
Andrew Hayes <pterodactal2000@outlook.com>
2014-06-16 21:51:55 +0930

    Added Chapter 1 Example

---
+++ src/Chapter1/Welcome.java
@@ -0,0 +1,15 @@
+package Chapter1;
+
+import javax.swing.*;
+
+/**
+ * Created by Andrew on 16-Jun-14.
+ * This program displays "Welcome to Java!" in a message dialog box
+ */
+public class Welcome
+{
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "Welcome to Java!", "Chapter 1 Example", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
